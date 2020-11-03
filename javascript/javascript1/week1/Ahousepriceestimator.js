ac4687a7a57cc93b3cb932301570ef1ef706b978
DlnyaMazhari <mazharidlnya@gmail.com>
2020-11-03 18:59:11 +0100

    the variable names are changed to CamelCase

--- javascript/javascript1/week1/Ahousepriceestimator.js
+++ javascript/javascript1/week1/Ahousepriceestimator.js
@@ -1,19 +1,19 @@
 // Project 3
 //A house price estimator
-let houseprice;
-let volumeinMeters;
+let housePrice;
+let volumeInMeters;
 
 // 1-peter's house price calculation
 let gardenSizeInM2 = 100;
 let width = 8;
 let depth = 10;
 let height = 10;
-let actualhouseprice = 2500000;
+let actualHousePrice = 2500000;
 
-volumeinMeters = width * depth * height;
-houseprice = volumeinMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+volumeInMeters = width * depth * height;
+housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
 
-if (actualhouseprice > houseprice) {
+if (actualHousePrice > housePrice) {
   console.log("NO Peter, you are paying too much");
 } else {
   console.log("Go for it Peter , it is reasonable;)");
@@ -25,12 +25,12 @@ gardenSizeInM2 = 70;
 width = 5;
 depth = 11;
 height = 8;
-actualhouseprice = 1000000;
+actualHousePrice = 1000000;
 
-volumeinMeters = width * depth * height;
-houseprice = volumeinMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+volumeInMeters = width * depth * height;
+housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
 
-if (actualhouseprice > houseprice) {
+if (actualHousePrice > housePrice) {
   console.log("NO Julia, you are paying too much");
 } else {
   console.log("Go for it Julia, it is reasonable;)");
