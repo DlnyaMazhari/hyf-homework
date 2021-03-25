eb5524751f2d441c119ba7247dab4057553976a4
DlnyaMazhari <mazharidlnya@gmail.com>
2021-03-25 11:06:35 +0100

    the recommended changes are implemented

--- react/week4/git-users/src/UserContext.js
+++ react/week4/git-users/src/UserContext.js
@@ -1,37 +1,60 @@
 import React, { useState, useEffect, createContext } from "react";
+import axios from "axios";
 
 export const userContext = createContext();
 
-const UserProvider = (props) => {
+const UserProvider = ({ children }) => {
   const [user, setUser] = useState([]);
   const [loading, setLoading] = useState(false);
   //   const [hasResult, setHasResult] = useState(true);
   const getUsers = async (elm) => {
-    // setLoading(true);
-    const url = `https://api.github.com/search/users?q=${elm}`;
-    const Api = await fetch(url)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const { items } = data;
-        let userList = [];
-        items.forEach((item) => {
-          const { login } = item;
-          userList.push(login);
-        });
-        if (data.total_count > 0) {
-          setUser(userList);
-        } else {
-          setUser(["No Results!"]);
-        }
-
-        setLoading(false);
-        console.log(data);
-      })
-      .catch((error) => {
-        console.log("error");
+    setLoading(true);
+    try {
+      const userData = await axios.get(
+        `https://api.github.com/search/users?q=${elm}`
+      );
+      const {
+        data: { items },
+        data: { total_count },
+      } = await userData;
+      let userList = [];
+      items.forEach((item) => {
+        const { login } = item;
+        userList.push(login);
       });
+      if (total_count > 0) {
+        setUser(userList);
+      } else {
+        setUser(["No Results!"]);
+      }
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+    // const url = `https://api.github.com/search/users?q=${elm}`;
+    // const Api = await fetch(url)
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    // .then((data) => {
+    //   const { items } = data;
+    //   let userList = [];
+    //   items.forEach((item) => {
+    //     const { login } = item;
+    //     userList.push(login);
+    //   });
+    //   if (data.total_count > 0) {
+    //     setUser(userList);
+    //   } else {
+    //     setUser(["No Results!"]);
+    //   }
+
+    //   setLoading(false);
+    //   console.log(data);
+    // })
+    // .catch((error) => {
+    //   console.log("error");
+    // });
   };
 
   const providerObj = {
@@ -41,9 +64,7 @@ const UserProvider = (props) => {
     setLoading,
   };
   return (
-    <userContext.Provider value={providerObj}>
-      {props.children}
-    </userContext.Provider>
+    <userContext.Provider value={providerObj}>{children}</userContext.Provider>
   );
 };
 
